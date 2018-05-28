10aa223462063705221a2c14f2f1e37924b6d098
Jianxiong Cai <caijx@shanghaitech.edu.cn>
2018-05-28 12:39:32 +0800

    LoopDetector: It is not working so far, the VOC comsume too much memory

--- src/loop-detector/LoopDetector.cpp
+++ src/loop-detector/LoopDetector.cpp
@@ -1,15 +1,23 @@
 
-#include <iostream>
-#include <opencv2/core/core.hpp>
-#include <opencv2/highgui/highgui.hpp>
-#include <DBoW2.h>
 #include "LoopDetector.h"
 
 LoopDetector::LoopDetector(SLAM_VIEWER::Map &map, const std::string& img_dir):
         img_dir_(img_dir) {
-    for (auto frame : map.GetAllKeyFrames()){
-        this->addKeyFrame(frame);
-    }
+//    for (auto frame : map.GetAllKeyFrames()){
+//        this->addKeyFrame(frame);
+//    }
+
+    voc_ = OrbVocabulary(9,3,DBoW2::TF_IDF, DBoW2::L1_NORM);
+    orb_ = cv::ORB::create();
+
+    // TODO: what if the img_dir does not have tailing '/'
+
+    auto features_all = computeAllFeatures(map,img_dir);
+
+    // create VOC
+    voc_.create(features_all);
+
+    int debug = 1;
 }
 
 bool LoopDetector::addKeyFrame(SLAM_VIEWER::KeyFrame * frame) {
@@ -20,17 +28,49 @@ bool LoopDetector::addKeyFrame(SLAM_VIEWER::KeyFrame * frame) {
         return false;
     }
 
-//    auto img = cv::imread(img_path);
+    auto img = cv::imread(img_path);
 //    cv::imshow("img",img);
 //    cv::waitKey(0);
-
-    // Extract ORB feature out
-
 
 
     // match against previous image seen before
 
 
     // Add ORB feature to DBoW2
+
+}
+
+std::vector<std::vector<cv::Mat>> LoopDetector::computeAllFeatures(SLAM_VIEWER::Map & map, const std::string & img_dir) {
+
+    std::vector<std::vector<cv::Mat>> features_all;
+    for (SLAM_VIEWER::KeyFrame* frame : map.GetAllKeyFrames()){
+        // read in the image
+        std::string img_path = img_dir_ + frame->GetFilename();
+        cv::Mat img = cv::imread(img_path);
+
+//        cv::imshow("img",img);
+//        cv::waitKey(0);
+
+        // compute ORB features
+        cv::Mat mask;
+        std::vector<cv::KeyPoint> keypoints;
+        cv::Mat descriptors;
+        orb_->detectAndCompute(img,mask,keypoints,descriptors);
+
+        std::cout << "descriptors: " << descriptors.rows << std::endl;
+
+        // change the structure of descriptors
+        std::vector<cv::Mat> features;
+        features.resize(descriptors.rows);
+        for(int i = 0; i < descriptors.rows; ++i)
+        {
+            features.push_back(descriptors.row(i));
+        }
+
+        // append the features of current image to features_all
+        features_all.push_back(features);
+    }
+
+    std::cout << "size of features_all: " << features_all.size() << std::endl;
 
 }
--- src/loop-detector/LoopDetector.h
+++ src/loop-detector/LoopDetector.h
@@ -3,16 +3,36 @@
 
 
 #include <Map.h>
+#include <iostream>
+
+// opencv
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/features2d.hpp>
+
+// DBoW
+#include <DBoW2.h>
+#include <BowVector.h>
 
 class LoopDetector {
 public:
+    // perform offline loop-detection from the loop detector
     LoopDetector(SLAM_VIEWER::Map&, const std::string&);
+
+private:
+    // iterate through all images and compute all features (ORB)
+    std::vector<std::vector<cv::Mat>> computeAllFeatures(SLAM_VIEWER::Map&, const std::string&);
 
     bool addKeyFrame(SLAM_VIEWER::KeyFrame *);
 
-private:
+
     // the directory to raw RGB image
     std::string img_dir_;
+
+    // TODO: tun the parameter of orb vocabulary
+    OrbVocabulary voc_;
+    cv::Ptr<cv::ORB> orb_;
+    std::vector<std::vector<cv::Mat>> features_;
 };
 
 
